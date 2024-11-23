b77d9d701ca3d32c1c0f6f67a3012879a90cb9b2
tal.aizenkraft <tal.aizenkraft@tufin.com>
2024-11-23 21:39:04 +0200

    ex1

--- Algebra.java
+++ Algebra.java
@@ -25,38 +25,60 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		/** this method gets two integers and returns the addition result of them */
+		int result = x1;
+		for (int i = 0; i < x2; i++) {
+			result++;
+		}
+		return result;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		/** this method gets two integers and returns the substracution of the second argument from the first */
+		int result = x1;
+		for (int i =0; i < x2; i++) {
+			result--;
+		}
+		return result;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		/** this method get two integers and returns the multiplication result */
+		int result = 0;
+		for (int i = 0; i < x2; i++) {
+			result += x1;
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		/** this method get two integers x and n and returns the result of x in the power of n*/
+		int result = x;
+		for (int i = 0; i < n - 1; i++) {
+			result *= x;
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		/** this func gets two intergers x1, x2 and returns the integer part of x1 / x2 */
+		int counter = 0;
+		int sum = 0;
+		while (sum + x2 <= x1) {
+			sum += x2;
+			counter ++;
+		}
+		return counter;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		/** this func gets two intergers x1, x2 and returns x1 % x2 */
+		return x1 - times(div(x1, x2), x2);
 	}	
 
 	// Returns the integer part of sqrt(x) 
